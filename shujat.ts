63098adb462bcf85b0bf78aae2408bb45bdaa402
oofarhanafarhat <khnkaneez@gmail.com>
2024-07-21 12:15:52 +0500

    Add files via upload
    type script journey

---
+++ shujat.ts
@@ -0,0 +1,28 @@
+let userName: string = "shujaat";
+let password: number = 235;
+let ispresent: boolean = true;
+let output: string = userName + " " + password + " " + ispresent;
+console.log(output);
+let Name: string = "kamran";
+let sName: string = "khan";
+let age: number = 35;
+let profession: string = "doctor";
+console.log(
+  `i m ${Name} ${sName},i m ${age} years old and i m a ${profession} ,i m availiable 5pm to 9pm at ghulshan e iqbal clinic`
+);
+let x = 5;
+let y = 10;
+let sum = x + y;
+console.log(sum);
+let x1 = 50;
+let y1 = 20;
+let result = x1 - y1;
+console.log(result);
+let q = 50;
+let p = 7;
+let R = q / p;
+console.log(R);
+let a = 30;
+let b = 4;
+let c = a % b;
+console.log(c);
