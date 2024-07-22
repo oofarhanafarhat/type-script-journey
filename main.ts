eba0679e5ea075af67edb494e6e15bc6e8ec5ae9
oofarhanafarhat <khnkaneez@gmail.com>
2024-07-22 10:15:48 +0500

    Add files via upload
    logical operators

--- main.ts
+++ main.ts
@@ -1,20 +1,31 @@
-// comparsion in typescript
-//   equall to
-// just value equal to duble == equal
-let class1 = 100;
-let student = 200;
-console.log(class1 == student);
-// type and value is equal to tuple =
-let class2 = "student";
-let class3 = "student";
-console.log(class2 === class3);
-// greter ten and equal <=
+// // logical operators
+// // logical operators three types hoty hain
+// // 1 and operator woh hoty hain jis men dono condition true ho tb he true karta hai ager ak true ak false ho to false result show hoga is ko define karny k liye dono condition k bech men && lagaya jata hai.
+// let a = 10;
+// let b = 10;
+// console.log(a == b && b <= a);
 
-let sindh = "sindh";
-let punjab = "punjab";
-console.log(sindh <= punjab);
-// if i use !<= in log then answer is true because !<= change answer to oppsite site
+// // 0r operators woh hoty hain jin ko true  karny k liye koi ak condition bhi true ho to ya chalta hai in ka sign|| hai
+// let c1 = 12;
+// let b1 = 20;
+// let c = c1 > b1 || b1 >= c1;
+// console.log(c);
 
-let kps = "1000";
-let bss = "2000";
-console.log(kps > bss);
+// // NOT operator (!) marks sy show hoty hain in ko define karny k liye koi ak condition bhi laga sakhty hain
+
+// let s1 = 100;
+// let s2 = !(s1 <= 50);
+// console.log(s2);
+// arthimatic assigment operators
+
+let num = 10;
+num += 8;
+console.log(num);
+
+let sheet = 30;
+sheet *= 3;
+console.log(sheet);
+
+let baskit = 100;
+baskit /= 4;
+console.log(baskit);
